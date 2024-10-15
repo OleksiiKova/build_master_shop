c7ebaf179b8779c6f0bd144e23fab1acbab81f50
Oleksii Kovalenko <a16kova@gmail.com>
2024-10-15 21:06:34 +0000

    Add adjust and remove cart views, and calc_subtotal template filter

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name="view_cart"),
     path('add/<sku>/', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<sku>/', views.adjust_cart, name='adjust_cart'),
+    path('remove/<sku>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -46,8 +46,32 @@ def add_to_cart(request, sku):
             'price': str(variant.price if variant else product.price),
         }
 
-
     request.session['cart'] = cart
 
     return redirect(redirect_url)
 
+
+def adjust_cart(request, sku):
+    
+    quantity = int(request.POST.get('quantity', 1))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[sku]['quantity'] = quantity
+    else:
+        cart.pop(sku)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, sku):
+    
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(sku)
+        request.session['cart'] = cart
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
