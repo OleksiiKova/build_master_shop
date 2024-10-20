225a24b4ff2d05e8c3d84d86be362fe2a58319e6
Oleksii Kovalenko <a16kova@gmail.com>
2024-10-20 16:34:55 +0000

    Add basic checkout functionality

--- build_master/settings.py
+++ build_master/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+if os.path.isfile('env.py'):
+    import env
 from decimal import Decimal
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -183,3 +185,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
