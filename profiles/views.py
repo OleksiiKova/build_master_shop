114d07dc518eda5065e6bc3a47a3c6bc54f6f0b2
Oleksii Kovalenko <a16kova@gmail.com>
2024-10-24 18:54:42 +0000

    Add profile form, views and updated template

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import UserProfile
-# from .forms import UserProfileForm
+from .forms import UserProfileForm
 
 from checkout.models import Order
 
@@ -14,9 +14,23 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile, 
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
