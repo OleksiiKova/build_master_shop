e182a2216a4202f97177f8b5424871c9c890b602
Oleksii Kovalenko <a16kova@gmail.com>
2024-10-27 15:10:20 +0000

    Product Admin - adjust image field

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-# from .widgets import CustomClearableFileInput
+from .widgets import CustomClearableFileInput
 from django.forms import inlineformset_factory
 from .models import Product, ProductVariant, FirstLevelCategory, SecondLevelCategory, ThirdLevelCategory
 
@@ -9,6 +9,8 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'description', 'first_level_category', 'second_level_category', 'third_level_category', 'price', 'stock', 'additional_attributes', 'image']
         
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
 class ProductVariantForm(forms.ModelForm):
     class Meta:
         model = ProductVariant
