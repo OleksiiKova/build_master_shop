b68954336ed35adfd5a7099c965b84dd1caaeb7c
Oleksii Kovalenko <a16kova@gmail.com>
2024-11-10 15:35:53 +0000

    Add the same margin to all main content

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .widgets import CustomClearableFileInput
 from django.forms import inlineformset_factory
 from .models import (
     Product, ProductVariant, FirstLevelCategory, SecondLevelCategory,
@@ -13,9 +12,6 @@ class ProductForm(forms.ModelForm):
         fields = ['name', 'description', 'first_level_category',
                   'second_level_category', 'third_level_category', 'price',
                   'stock', 'additional_attributes', 'image',]
-
-    image = forms.ImageField(
-        label='Image', required=False, widget=CustomClearableFileInput)
 
 
 class ProductVariantForm(forms.ModelForm):
